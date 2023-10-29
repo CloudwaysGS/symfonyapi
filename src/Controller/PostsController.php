f5e1696092fb7212418a6df5ee51e4697a1636ec
Abdou Karim Coulibaly <98843343+absrimka@users.noreply.github.com>
2023-10-29 01:52:39 +0000

    second commit

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -10,9 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PostsController extends AbstractController
 {
